860c9a884ec4ad3baed73bc4d845c31ff876d6f8
Christophe Ha <christophe.ha1@gmail.com>
2021-01-26 21:16:19 +0100

    Minor visual change on playerList component

--- src/PlayerList.svelte
+++ src/PlayerList.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
   import { playerStore, playerId } from './store';
   $: players = $playerStore;
+
+  function formatName(playerId: string, currentPlayer: string) {
+    return playerId === currentPlayer 
+      ? `<b> ${currentPlayer} <b>`
+      : currentPlayer;
+  }
 </script>
 
 <main>
-  <h1> My id {$playerId} </h1>
-  <h2> Connected players </h2>
+  <h3> Connected players </h3>
   <ul>
     {#each players as player}
-      <li> {player} </li>
+      <li> {@html formatName($playerId, player)} </li>
     {/each}
   </ul>
 </main>
+
+<style>
+  li {
+    text-align: left;
+  }
+</style>
