4dae224f082ac9c335de5f00a9e79f3cea99643d
Christophe Ha <christophe.ha1@gmail.com>
2021-02-11 19:27:49 +0100

    Refactor Lobby component

--- src/Lobby.svelte
+++ src/Lobby.svelte
@@ -1,33 +1,30 @@
 <script lang="ts">
-
-  import { fly } from 'svelte/transition';
-	import NameInput from "./NameInput.svelte";
-	import PlayerList from "./PlayerList.svelte";
+  import { fly } from "svelte/transition";
+  import NameInput from "./NameInput.svelte";
+  import PlayerList from "./PlayerList.svelte";
   import Settings from "./Settings.svelte";
   import StartButton from "./StartButton.svelte";
-	import { playerId } from "./store.js";
-  import statefulSwap from "./StatefulSwap"
+  import { playerId } from "./store.js";
+  import statefulSwap from "./StatefulSwap";
 
-  const { onOutro, transitionTo, state } = statefulSwap("init");
+  const { onOutro, transitionTo, state } = statefulSwap($playerId);
 
   $: if ($playerId) {
     transitionTo($playerId);
   }
-
 </script>
 
 <main>
-	{#if $state === "init"}
-    <div out:fly="{{ y: 500, duration: 300 }}" on:outroend={onOutro}>
+  {#if $state === ""}
+    <div out:fly={{ x: -500, duration: 200 }} on:outroend={onOutro}>
       <NameInput />
     </div>
-	{/if}
-  {#if $state === $playerId}
-    <div in:fly="{{ y: 500, duration: 500 }}" on:outroend={onOutro}>
+  {:else}
+    <div in:fly={{ x: 500, duration: 1000 }} on:outroend={onOutro}>
       <Settings />
-      <br>
+      <br />
       <StartButton />
-      <br>
+      <br />
       <PlayerList />
     </div>
   {/if}
