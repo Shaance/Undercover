ab778588ffb98b6c9b63824ff784c92a64be3ea4
Shaance <christophe.ha1@gmail.com>
2021-03-07 16:18:56 +0100

    Added room id in footer

--- src/Lobby.svelte
+++ src/Lobby.svelte
@@ -4,7 +4,7 @@
   import PlayerList from "./PlayerList.svelte";
   import Settings from "./Settings.svelte";
   import StartButton from "./StartButton.svelte";
-  import { playerId } from "./store.js";
+  import { playerId, roomId } from "./store.js";
   import statefulSwap from "./StatefulSwap";
 
   const { onOutro, transitionTo, state } = statefulSwap($playerId);
@@ -20,12 +20,31 @@
       <NameInput />
     </div>
   {:else}
-    <div in:fly={{ x: 500, duration: 1000 }} on:outroend={onOutro}>
+    <div class ="content" in:fly={{ x: 500, duration: 1000 }} on:outroend={onOutro}>
       <Settings />
       <br />
       <StartButton />
       <br />
       <PlayerList />
     </div>
+    <footer> Room ID: {$roomId} </footer>
   {/if}
 </main>
+
+<style>
+  main {
+    position: relative;
+    min-height: 100vh;
+  }
+
+  .content {
+    padding-bottom: 6.5rem;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 6.5rem;
+  }
+</style>
