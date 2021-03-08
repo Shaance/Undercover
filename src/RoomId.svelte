7f27c39220bd0c6606be6860dfa7ad4bbf075bad
Shaance <christophe.ha1@gmail.com>
2021-03-08 13:46:27 +0100

    Added copy to clipboard for room id

--- src/RoomId.svelte
+++ src/RoomId.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
-    import { roomId } from "./store.js";
+  import { roomId } from "./store.js";
+
+  function copyRoomId() {
+    navigator.clipboard.writeText($roomId)
+    .then(
+      () => alert("Copied room ID to clipboard!"),
+      (err) => console.error("Async: Could not copy text: ", err)
+    );
+  }
 </script>
 
 <main>
-    <h2>Room ID: {$roomId}</h2>
+  <h2>Room ID: {$roomId} &nbsp;</h2>
+  <button on:click={copyRoomId}>
+    <i class="far fa-copy"/>
+  </button>
 </main>
 
 <style>
-    h2 {
-        color: darkslateblue;
-        font-size: 1.2em;
-        font-weight: 150;
-    }
+  h2 {
+    display: inline-block;
+    color: darkslateblue;
+    font-size: 1.2em;
+    font-weight: 150;
+  }
+
+  button {
+    border: 0;
+  }
+
+  button:hover {
+	background-color:rgb(202, 202, 235);
+}
 </style>
